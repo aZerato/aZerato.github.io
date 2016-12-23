e3097ab50511bf7da6bf18e68a71dda2e176e8d1
aZerato <aZerato.graphe@gmail.com>
2016-12-23 17:53:59 +0100

    flickr component correction

--- app/flickr/flickr.component.js
+++ app/flickr/flickr.component.js
@@ -23,41 +23,34 @@
 			self.imgs = [];
 			self.imgsLoaded = false;
 
-			try
+			var flickrPhotos = $cookies.get('flickr-photos');
+			if(flickrPhotos === '' || flickrPhotos === undefined)
 			{
-				var flickrPhotos = $cookies.get('flickr-photos');
-				if(flickrPhotos === '' || flickrPhotos === undefined)
-				{
-					flickrService.get($http, $q, $sce)
-					.then(function(flickrObj) {
-						self.username = flickrObj.username;
-						self.imgs = flickrObj.photos;
-						self.imgsLoaded = true;
-
-						flickrPhotos = {
-							photos: flickrObj.photos,
-							username: flickrObj.username
-						};
-
-						if(self.useCookies)
-						{
-							var expireDate = new Date();
-							expireDate.setMinutes(expireDate.getMinutes() + 5);
-							$cookies.put('flickr-photos', JSON.stringify(flickrPhotos), {'expires': expireDate});
-						}
-					});
-				}
-				else
-				{
-					self.username = flickrPhotos.username;
-					self.imgs = flickrPhotos.photos;
+				flickrService.get($http, $q, $sce)
+				.then(function(flickrObj) {
+					self.username = flickrObj.username;
+					self.imgs = flickrObj.photos;
 					self.imgsLoaded = true;
-				}
+
+					flickrPhotos = {
+						photos: flickrObj.photos,
+						username: flickrObj.username
+					};
+
+					if(self.useCookies)
+					{
+						var expireDate = new Date();
+						expireDate.setMinutes(expireDate.getMinutes() + 5);
+						$cookies.put('flickr-photos', JSON.stringify(flickrPhotos), {'expires': expireDate});
+					}
+				});
 			}
-			catch(error)
+			else
 			{
-				console.log('flickrComponent::flickrController::Error (' + error + ')');
-				self.imgsLoaded = false;
+				flickrPhotos = JSON.parse(flickrPhotos);
+				self.username = flickrPhotos.username;
+				self.imgs = flickrPhotos.photos;
+				self.imgsLoaded = true;
 			}
 		};
 
